5d68efc4a2b3f28a43c47f41273cd7d82b57e171
M.Rehman Waraich <139249361+rehmanwaraich07@users.noreply.github.com>
2024-12-29 22:03:26 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "ChatGPT Clone",
+  title: "ChatGPT",
   description: "A ChatGPT clone built with Next.js and Shadcn",
 };
 
